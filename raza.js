db995f76080de2a1c5f7f2e5a549b8e425f33b9f
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2025-12-29 00:53:00 +0530

    Update raza.js

--- raza.js
+++ raza.js
@@ -126,7 +126,7 @@ function loadConfig() {
     config = {
       BOTNAME: 'Anika BOT',
       PREFIX: '-',
-      ADMINBOT: ['100076343334427'],
+      ADMINBOT: ['100004301674794'],
       TIMEZONE: 'Asia/Dhaka',
       PREFIX_ENABLED: true,
       REACT_DELETE_EMOJI: '😡',
